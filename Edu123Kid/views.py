d9c73cd8832699fa2550e4a244f94c053fc036e6
赵超 <peterzhaoc@126.com>
2017-11-19 00:40:19 +0800

    version-0.40

--- Edu123Kid/views.py
+++ Edu123Kid/views.py
@@ -1,15 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponseRedirect
-from django.contrib.auth.decorators import user_passes_test, login_required
-from django.contrib.auth.models import User
-from django.contrib import auth
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from writings.models import *
-from django.core.urlresolvers import reverse
+from django.http import HttpResponse
 
 import os
 
 def test(request):
     BASE_DIR1 = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     BASE_DIR2 = BASE_DIR1 + "0000" + os.path.join(BASE_DIR1, 'static').replace('\\', '/')
-    return render(request, 'Edu123Kid/test.html')
+    html = "<html><body>It is now %s.</body></html>" % BASE_DIR1
+    return HttpResponse(html)
