5107c8aed015c2a6188280573b06eca59b43c9bc
赵超 <peterzhaoc@126.com>
2017-11-18 22:37:49 +0800

    version-0.23

--- Edu123Kid/urls.py
+++ Edu123Kid/urls.py
@@ -2,9 +2,10 @@ from django.conf.urls import url, include
 from django.contrib.staticfiles import views
 from django.contrib import admin
 from django.conf import settings
+from profiles.views.index as index
 
 urlpatterns = [
-    url(r'^$', 'profiles.views.index', name='index'),
+    url(r'^$', index, name='index'),
     url(r'^admin/', admin.site.urls),
     url(r'', include('writings.urls')),
     url(r'', include('profiles.urls')),
