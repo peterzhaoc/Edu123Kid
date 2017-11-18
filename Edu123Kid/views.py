5b1080d00b97438a5e513c65240e482fe7704e43
赵超 <peterzhaoc@126.com>
2017-11-19 00:41:11 +0800

    version-0.41

--- Edu123Kid/views.py
+++ Edu123Kid/views.py
@@ -5,5 +5,5 @@ import os
 def test(request):
     BASE_DIR1 = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     BASE_DIR2 = BASE_DIR1 + "0000" + os.path.join(BASE_DIR1, 'static').replace('\\', '/')
-    html = "<html><body>It is now %s.</body></html>" % BASE_DIR1
+    html = "<html><body>It is now %s.</body></html>" % BASE_DIR2
     return HttpResponse(html)
