2fa22fc41eaa0edbd05e7b45b768ed4d0c3f6669
peterzhaoc <peterzhaoc@126.com>
2018-01-15 00:08:16 +0800

    v0.63

--- writings/views.py
+++ writings/views.py
@@ -84,14 +84,11 @@ def writing_task_detail(request,d):
         mentorprofile = None
     else:
         mentorprofile = MentorProfile.objects.get(userprofile=profile)
-        print writing_task.state
-        print writing_task.editor
-        print mentorprofile
         if writing_task.state == 2 and writing_task.editor == mentorprofile:
             submit = True
         elif writing_task.state == 3 and writing_task.finaleditor == mentorprofile:
             submit = True
-
+    print submit
 
     if request.method == 'POST':
         if writing_task.state == 2:
