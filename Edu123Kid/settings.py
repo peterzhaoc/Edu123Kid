d05199303a85801bcdb4e8eb743a694deef538b3
peterzhaoc <peterzhaoc@126.com>
2018-01-15 18:25:12 +0800

    v0.73

--- Edu123Kid/settings.py
+++ Edu123Kid/settings.py
@@ -139,9 +139,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static').replace('\\', '/')
-
+print STATIC_ROOT
 STATICFILES_DIRS =(os.path.join(BASE_DIR, 'common_static'),)
-
+print STATICFILES_DIRS
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
