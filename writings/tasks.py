3696a64a6d6c47654145167f170a005b52486e8d
peterzhaoc <peterzhaoc@126.com>
2018-03-23 22:35:59 +0800

    v1.02

--- writings/tasks.py
+++ writings/tasks.py
@@ -21,7 +21,7 @@ app.conf.update(
                 CELERYBEAT_SCHEDULE = {
                 'check_due_writing_tasks': {
                 'task': 'writings.tasks.writing_task_expire_remind',
-                'schedule': crontab(hour=22, minute=35),
+                'schedule': crontab(hour=22, minute=40),
                 },
                 }
                 )
