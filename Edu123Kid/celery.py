0f2ac5e8315f870b04ca854b90800e20267ce75b
peterzhaoc <peterzhaoc@126.com>
2018-04-01 23:30:43 +0800

    v1.10

--- Edu123Kid/celery.py
+++ Edu123Kid/celery.py
@@ -2,13 +2,14 @@
 from __future__ import absolute_import
 import os
 import django
-from celery import Celery
+from celery import Celery,platforms
 from django.conf import settings
 from django.apps import apps
 from datetime import datetime,timedelta
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Edu123Kid.settings')
 django.setup()
+platforms.C_FORCE_ROOT = True
 app = Celery('Edu123Kid')
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: [n.name for n in apps.get_app_configs()])
